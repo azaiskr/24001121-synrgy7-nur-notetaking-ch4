9388e9b4b5645e9ec060e7af9bfdceed4c98cd68
azaiskr <nuraziss557@gmail.com>
2024-05-10 16:41:35 +0700

    VM DI DATABASE [config]

--- app/src/main/java/com/synrgy/notetaking/data/database/User.kt
+++ app/src/main/java/com/synrgy/notetaking/data/database/User.kt
@@ -3,19 +3,14 @@ package com.synrgy.notetaking.data.database
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Entity
 @Parcelize
 data class User(
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    val id: Int = 0,
-
     @ColumnInfo(name = "username")
-    val username: String? = null,
+    val username: String,
 
     @ColumnInfo(name = "password")
-    val password: String? = null,
+    val password: String,
 ) : Parcelable
